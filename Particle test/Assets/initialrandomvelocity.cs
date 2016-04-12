70a61fc255d560afce4d11d25490790716e296cb
COASTER19 <mvrablic@mit.edu>
2016-04-12 02:51:11 -0400

    Added Unity project
    The folder should contain the entire project, to be opened in Unity 5.
    The particletest4.apk file is a compiled version of it which can be
    downloaded and installed on any android phone or tablet.

---
+++ Particle test/Assets/initialrandomvelocity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class initialrandomvelocity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2 ((Random.value - .5f)*4, (Random.value - .5f)*4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Particle test/Assets/mousemove.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+  var ray : Ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+  // 10 meters in front of the camera:
+  transform.position = ray.GetPoint(10) ;
+}
